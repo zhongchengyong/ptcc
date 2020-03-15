19c3e43cb17d88c15c2b99970f9ca07a505dddaf
钟乘永 <zhongcy93@gmail.com>
2020-03-15 21:16:07 +0800

    Init

---
+++ src/node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 钟乘永 on 2020/3/15.
+//
+
+#include "node.h"
---
+++ src/node.h
@@ -0,0 +1,14 @@
+//
+// Created by 钟乘永 on 2020/3/15.
+//
+
+#ifndef TCC_NODE_H
+#define TCC_NODE_H
+
+
+class node {
+
+};
+
+
+#endif //TCC_NODE_H
